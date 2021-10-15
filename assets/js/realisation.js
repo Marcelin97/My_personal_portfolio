c7ab643c940f1715622fc0cebe97ea1e11902e5a
Loïs <lois_m@outlook.com>
2021-10-15 12:28:58 +0200

    delete slide with scroll horizontal and add card with text hidden

--- assets/js/realisation.js
+++ assets/js/realisation.js
@@ -113,24 +113,9 @@ cursor.init();
 // Cursor
 //=============================>>
 
-const spaceHolder = document.querySelectorAll(".space-holder");
-const horizontal = document.querySelectorAll(".horizontal");
-const container = document.querySelectorAll(".container");
-const sticky = document.querySelectorAll(".sticky");
+// Projet card
+//=============================>>
 
-function calcDynamicHeight(ref) {
-  const vw = window.innerWidth;
-  const vh = window.innerHeight;
-  const objectWidth = ref.scrollWidth;
-  return objectWidth - vw + vh + 150;
-}
 
-container.forEach(function (current, i) {
-  spaceHolder[i].style.height = `${calcDynamicHeight(horizontal[i])}px`;
-  window.addEventListener("scroll", () => {
-    horizontal[i].style.transform = `translateX(-${sticky[i].offsetTop}px)`;
-  });
-  window.addEventListener("resize", () => {
-    spaceHolder[i].style.height = `${calcDynamicHeight(horizontal[i])}px`;
-  });
-});
+// End of projet card
+//=============================>>
